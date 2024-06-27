f08e37f720814f3770b8597d7334538ae57535c8
Kitso Mogale <kitsomogale19@gmail.com>
2024-06-27 15:25:58 +0200

    idk

--- vite-project/src/articles/Iot.jsx
+++ vite-project/src/articles/Iot.jsx
@@ -1,11 +1,12 @@
 
 export const Iot = ()=>{
     return(
-        <div id='iot'>
-          <h1>
+        <>
+         <h1>
           Programming the physical world 
           </h1>
 
+        <div id='iot'>
           <main>
              <p>
                 Calculating things such as velocity, acceleration, temperature, humidity etc requires inputs from dedicated electronic devices.
@@ -18,11 +19,29 @@ export const Iot = ()=>{
              </p>
 
              <p>
-             Meaning you could still do majority of the programming in a nodesjs server, and decide what should happen when certain input data is received. One person may also ask how a plain electronic device like an arduino can communicate with a server. This is made possible by other electronic devices called sensors or electronic modules.  
+             Meaning you could still do majority of the programming in a nodesjs server, and decide what should happen when certain input data is received. One person may also ask how a plain electronic device like an arduino can communicate with a server. This is made possible by other electronic devices called sensors or electronic modules.  <br/>
+             Internet of Things (IoT) sensors play a crucial role in connecting the physical world to the digital world, enabling a wide range of applications that enhance efficiency, safety, and convenience. Choosing the right sensor and communication protocol depends on the specific requirements of the application, such as range, data rate, power consumption, and environmental conditions.
              </p>
-             
+            
           </main>
 
         </div>
+        <div id='conclusion'>
+          <p id='wifi_'>This is a wifi module, it helps the arduino connect to the internet if available. 
+            <br/>
+          Pros: High data rates, wide coverage.
+          Cons: High power consumption, potential interference.
+           </p>
+          <img id='wifi' src='/Screenshot_20240627_134103.jpg'/>
+        </div>
+
+        <div className="conclusion">
+            <p>These are various other modules used as input devices to the arduino.<br/>
+               These include temperature sensor, object detector, distance module and water sensor.
+             </p>
+
+        </div>
+
+        </>
     )
 }
