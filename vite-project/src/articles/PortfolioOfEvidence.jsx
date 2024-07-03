30c5ee6a5651362632245d446ec9b1411ef21ff1
Kitso Mogale <kitsomogale19@gmail.com>
2024-07-03 23:47:27 +0200

    kjcnv

--- vite-project/src/articles/PortfolioOfEvidence.jsx
+++ vite-project/src/articles/PortfolioOfEvidence.jsx
@@ -9,7 +9,7 @@ export const PortfolioOfEvidence = ()=>{
            <div>
             <p>
                <span>W</span>hen looking for employment , software developers in particular, myself included, tend to think companies , whether private or government owned, are obligated to fairly review their status and skills ,and try by all means to give them the job opportunity.
-                They have the "backend" idea that companies exist only to serve their needs. This holds to be true to an extent with our fields of employment. That is why you can find people who are already employed , going on a strike for the sake of salary or wage increase. They do this with the mindset that the company can and should serve their needs. <b>Don't get me wrong , I do think fair wages and salaries are of essence when talking about a growing economy</b>, but obligation towards companies shouldn't be the deciding factor.The deciding factor should be the obligation of the job seekers to be equiped with the above average skills.
+                They have the "backend" idea that companies exist only to serve their needs. This holds to be true to an extent with our fields of employment. That is why you can find people who are already employed , going on a strike for the sake of salary or wage increase. They do this with the mindset that the company can and should serve their needs. <b>Don't get me wrong , I do think fair wages and salaries are of essence when talking about a growing economy</b>, but obligation towards companies shouldn't be the deciding factor.The deciding factor should be the obligation of the job seekers to be equiped with the above average skills required.
             </p>
             <img src='./download (10).jpg'/>
             <div>
@@ -23,5 +23,7 @@ export const PortfolioOfEvidence = ()=>{
         </>
     )
 }
+
+
 
 
