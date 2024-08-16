93a9af34f0a973ec99a30db0f39734b887504c18
Kitso Mogale <kitsomogale19@gmail.com>
2024-08-16 10:38:25 +0200

    tbontb

--- vite-project/src/articles/ToBeOrNotToBe.jsx
+++ vite-project/src/articles/ToBeOrNotToBe.jsx
@@ -4,13 +4,55 @@ export const TBONTB = ()=>{
          <h1>
           To Be Or Not To Be
           </h1>
+          <p><i>To be self-Taught or not to be self-taught. To be self-employed or to not be self-employed</i></p>
 
         <div id='TBONTB'>
           <main>
-            <h2>To be a graduate software developer or be a self-taught software developer.</h2>
+            <h2>1. To be a graduate software developer or be a self-taught software developer.</h2>
             <div>
-                <p>When deciding to persue a career in software development, in this day and age one has one of two options, go to a recognized university or learn on the internet "How to become a self-taught software developer for beginers".Both paths have their own pro and cons that can sometimes be relative to each individual.</p>
-                <p>Making a decision to these career paths,may seem binary, "choose one and let go of one forever",but realistically, the choice is analoguous,"You can choose one and follow the other one later if, and not limited to if, the first one was not as productive as you would have liked or as the economy would have liked.</p>
+                <p className="introTBONTB">When deciding to persue a career in software development, in this day and age one has one of two options, go to a recognized university or learn on the internet "How to become a self-taught software developer for beginers".Both paths have their own pros and cons that can sometimes be relative to each individual.</p>
+                <p className="introTBONTB">Making a decision to these career paths,may seem binary, "choose one and let go of one forever",but realistically, the choice is analoguous,"You can choose one and follow the other one later if, and not limited to if, the first one was not as productive as you would have liked or as the economy would have liked.This is also relative to the individual,one might have a lot of time in their hands to complete both routes and the next may not have such a magnitude of time to complete both but limited to one.The next person may not have the same magnitude of time but can learn at a faster rate and assumes the ability to finish both in half the time.It's all relative.</p>
+
+                <h4>The graduate route</h4>
+                <p className="introTBONTB">Taking a course in university for a career in software development has been the most recomended route.One can choose to take the Computer Science course(<i>cs</i>) or not to take the cs course, to take the computer engineering course or not to take the computer engineering course.Either way of all the ways, the outcome(<i>degree relating to software development</i>) is highy desired by the 'theoretical' economy.</p>
+                 <p><b>Pros:</b></p>
+                <ul>
+                    <li><i>Highest level of understanding the core concepts relating to software development.</i></li>
+                    <li><i>Taught face to face by someone who understands all the contents</i></li>
+                    <li><i>Journey is made clear by the university step-by-step</i></li>
+                    <li><i>Access to peers who are pursuing the same goal.</i></li>
+                    <li><i>Certification</i></li>
+                </ul>
+
+                <p><b>Cons:</b></p>
+                <ul>
+                    <li><i>3 year course may or may not take 3 years but 4 or 5 years</i></li>
+                    <li><i>Meet submision deadline not understanding deadline </i></li>
+                    <li><i>Economy may claim your certificate is useless</i></li>
+                    <li><i>Heavy reading and heavy testing </i></li>
+                    <li><i>High costs</i></li>
+                    
+                    
+                </ul>
+
+                <h4>The self-taught route</h4>
+
+                <p className="introTBONTB">Self-taught is self-explanitory. You choose the contents you want to learn and when to learn them.</p>
+
+                <p><b>Pros:</b></p>
+                <ul>
+                    <li><i></i></li>
+                    <li><i>Own timing</i></li>
+                    <li><i>No deadline</i></li>
+                    <li><i>Internet offers free content</i></li>
+                </ul>
+
+                <p><b>Cons:</b></p>
+                <ul>
+                    <li><i>No structure</i></li>
+                    <li><i>Leopard in a jungle</i></li>
+                    <li><i>No certification</i></li>
+                </ul>
             </div>
           </main>
 
